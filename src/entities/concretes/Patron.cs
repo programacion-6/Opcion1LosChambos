5242a98f935dc5e2959849e1666a42d8d0b624bf
Leonardo Herrera <leonardo.herrera@fundacion-jala.org>
2024-08-19 09:19:46 -0400

    feat: entities creation

---
+++ src/entities/concretes/Patron.cs
@@ -0,0 +1,21 @@
+public class Patron : IEntity
+{
+    public Guid Id { get; }
+    public string Name { get; set;}
+    public int MembershipNumber { get; set;}
+    public string ContactDetails { get; set;}
+
+    public Patron(string name, int membershipNumber, string contactDetails)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        MembershipNumber = membershipNumber;
+        ContactDetails = contactDetails;
+    }
+
+    public void BorrowBook(Book book)
+    {}
+
+    public void ReturnBook(Book book)
+    {}
+}
