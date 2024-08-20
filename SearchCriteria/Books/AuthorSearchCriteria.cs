7d61cff64d2d054255ddea22a88e4cb21b7ac120
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-08-19 22:31:45 -0400

    feat: implement book concretes search by classes

---
+++ SearchCriteria/Books/AuthorSearchCriteria.cs
@@ -0,0 +1,18 @@
+﻿using LosChambos.entities.concretes;
+
+namespace LosChambos.SearchCriteria.Books;
+
+public class AuthorSearchCriteria : ISearchCriteria<Book>
+{
+    public string Author { get; set; }
+
+    public AuthorSearchCriteria(string author)
+    {
+        Author = author;
+    }
+
+    public bool Matches(Book book)
+    {
+        return book.Author.Contains(Author, StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ SearchCriteria/Books/ISBNSearchCriteria.cs
@@ -0,0 +1,18 @@
+﻿using LosChambos.entities.concretes;
+
+namespace LosChambos.SearchCriteria.Books;
+
+public class ISBNSearchCriteria : ISearchCriteria<Book>
+{
+    public string ISBN { get; set; }
+
+    public ISBNSearchCriteria(string isbn)
+    {
+        ISBN = isbn;
+    }
+
+    public bool Matches(Book book)
+    {
+        return book.ISBN.Equals(ISBN, StringComparison.OrdinalIgnoreCase);
+    }
+}
