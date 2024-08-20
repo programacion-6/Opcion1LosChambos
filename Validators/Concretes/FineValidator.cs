f346115a24c974712090cb57404c658bb29835a3
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-08-19 23:23:39 -0400

    feat: implement validators concrete classes

---
+++ Validators/Concretes/FineValidator.cs
@@ -0,0 +1,24 @@
+﻿using LosChambos.Entities.Concretes;
+
+namespace LosChambos.Validators.Concretes;
+
+public class FineValidator : Validator<Fine>
+{
+    protected override void InitializeValidations()
+    {
+        Validations.Add("Patron is required.", fine => fine.Patron != null);
+        Validations.Add("Amount must be greater than 0.", fine => fine.Amount > 0);
+        Validations.Add(
+            "Due date must be set if fine is not paid.",
+            fine => !fine.Paid || fine.DueDate != null
+        );
+        Validations.Add(
+            "Paid date must be set if fine is paid.",
+            fine => !fine.Paid || fine.PaidDate != null
+        );
+        Validations.Add(
+            "Paid date cannot be before due date.",
+            fine => !fine.Paid || fine.PaidDate >= fine.DueDate
+        );
+    }
+}
---
+++ Validators/Concretes/ReservationValidator.cs
@@ -0,0 +1,24 @@
+﻿using LosChambos.Entities.Concretes;
+
+namespace LosChambos.Validators.Concretes;
+
+public class ReservationValidator : Validator<Reservation>
+{
+    protected override void InitializeValidations()
+    {
+        Validations.Add("Patron is required.", reservation => reservation.Patron != null);
+        Validations.Add("Book is required.", reservation => reservation.Book != null);
+        Validations.Add(
+            "Expiry date must be set.",
+            reservation => reservation.ExpiryDate != default
+        );
+        Validations.Add(
+            "Reservation date must be set.",
+            reservation => reservation.ReservationDate != default
+        );
+        Validations.Add(
+            "Reservation date must be before or equal to expiry date.",
+            reservation => reservation.ReservationDate <= reservation.ExpiryDate
+        );
+    }
+}
