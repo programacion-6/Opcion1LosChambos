25d0c4a6444ee97a856a01f3730bf3fda7b57468
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-08-19 23:07:56 -0400

    feat: implement patron concretes search by classes

---
+++ SearchCriteria/Patrons/MembershipNumberSearchCriteria.cs
@@ -0,0 +1,18 @@
+﻿using LosChambos.Entities.Concretes;
+
+namespace LosChambos.SearchCriteria.Patrons;
+
+public class MembershipNumberSearchCriteria : ISearchCriteria<Patron>
+{
+    public int MembershipNumber { get; set; }
+
+    public MembershipNumberSearchCriteria(int membershipNumber)
+    {
+        MembershipNumber = membershipNumber;
+    }
+
+    public bool Matches(Patron patron)
+    {
+        return patron.MembershipNumber == MembershipNumber;
+    }
+}
---
+++ SearchCriteria/Patrons/NameSearchCriteria.cs
@@ -0,0 +1,18 @@
+﻿using LosChambos.Entities.Concretes;
+
+namespace LosChambos.SearchCriteria.Patrons;
+
+public class NameSearchCriteria : ISearchCriteria<Patron>
+{
+    public string Name { get; set; }
+
+    public NameSearchCriteria(string name)
+    {
+        Name = name;
+    }
+
+    public bool Matches(Patron patron)
+    {
+        return patron.Name.Contains(Name, StringComparison.OrdinalIgnoreCase);
+    }
+}
