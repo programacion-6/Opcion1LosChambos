4b873c87f15febee2da82033741124473f186cfd
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-08-20 01:17:50 -0400

    feat: add SearchMenuInterface static methods

---
+++ UInterface/Menu/SearchMenuUInterface.cs
@@ -0,0 +1,50 @@
+﻿using LosChambos.Entities;
+using LosChambos.Managers;
+using LosChambos.SearchCriteria;
+
+namespace LosChambos.UInterface.Menu;
+
+public class SearchMenuUInterface<TEntity>
+    where TEntity : IEntity
+{
+    public static void ShowSearchedData<TCriteria>(
+        string inputPrompt,
+        Func<string, TCriteria?> createCriteria,
+        AManager<TEntity> manager
+    )
+        where TCriteria : ISearchCriteria<TEntity>
+    {
+        var input = UserInterface.GetUserInput(inputPrompt);
+        TCriteria? criteria = createCriteria(input);
+
+        if (criteria == null)
+        {
+            UserInterface.ShowMessage("Invalid criteria format.");
+            return;
+        }
+
+        var results = manager.Search(criteria);
+        UserInterface.DisplayListResult(results);
+    }
+
+    public static IEntity? ShowSearchedItemById(AManager<TEntity> manager, string titleInstruction)
+    {
+        var idInput = UserInterface.GetUserInput($"Enter {titleInstruction} ID: ");
+        if (Guid.TryParse(idInput, out var id))
+        {
+            var item = manager.GetItemById(id);
+            if (item != null)
+            {
+                UserInterface.ShowMessage(item.ToString() ?? "No item found");
+                return item;
+            }
+            else
+                UserInterface.ShowMessage("No item found");
+        }
+        else
+        {
+            UserInterface.ShowMessage("Invalid ID format.");
+        }
+        return null;
+    }
+}
