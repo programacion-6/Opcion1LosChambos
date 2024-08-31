45517b0cc82eeaba8626ab8128cfcd6eb3425b46
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2024-08-30 23:08:21 -0400

    refactor: borrowingTransaction class to improve encapsulation

--- Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/BorrowingTransaction.cs
+++ Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/BorrowingTransaction.cs
@@ -3,34 +3,69 @@ namespace LosChambos.Entities.Concretes;
 public class BorrowingTransaction : IEntity
 {
     public Guid Id { get; }
-    public Book Book;
-    public Patron Patron;
-    public DateTime BorrowedDate;
-    public DateTime DueDate;
-    public DateTime? ReturnedDate;
-    public Fine? Fine;
-    public bool Returned;
+    private Book _book;
+    private Patron _patron;
+    private DateTime _borrowedDate;
+    private DateTime _dueDate;
+    private DateTime? _returnedDate;
+    private Fine? _fine;
+    private bool _returned;
 
     public BorrowingTransaction(Book book, Patron patron, DateTime dueDate)
     {
         Id = Guid.NewGuid();
-        Book = book;
-        Patron = patron;
-        DueDate = dueDate;
-        BorrowedDate = DateTime.Now;
-        Returned = false;
+        _book = book;
+        _patron = patron;
+        _dueDate = dueDate;
+        _borrowedDate = DateTime.Now;
+        _returned = false;
     }
 
+    public Book Book 
+    { 
+        get => _book; 
+        set => _book = value; 
+    }
+    public Patron Patron 
+    { 
+        get => _patron; 
+        set => _patron = value; 
+    }
+    public DateTime BorrowedDate 
+    { 
+        get => _borrowedDate; 
+        set => _borrowedDate = value; 
+    }
+    public DateTime DueDate 
+    { 
+        get => _dueDate; 
+        set => _dueDate = value; 
+    }
+    public DateTime? ReturnedDate 
+    { 
+        get => _returnedDate; 
+        set => _returnedDate = value; 
+    }
+    public Fine? Fine 
+    { 
+        get => _fine; 
+        set => _fine = value; 
+    }
+    public bool Returned 
+    { 
+        get => _returned; 
+        set => _returned = value; 
+    }
     public void BorrowBook()
     {
-        BorrowedDate = DateTime.Now;
-        Returned = false;
+        _borrowedDate = DateTime.Now;
+        _returned = false;
     }
 
     public void ReturnBook()
     {
-        Returned = true;
-        ReturnedDate = DateTime.Now;
+        _returned = true;
+        _returnedDate = DateTime.Now;
     }
 
     public override string ToString()
