2fe52ff49142946150b1725f957549db918ba82e
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2024-09-02 04:01:09 -0400

    refactor: fine class to improve encapsulation

--- Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/Fine.cs
+++ Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/Fine.cs
@@ -2,33 +2,59 @@ namespace LosChambos.Entities.Concretes;
 public class Fine : IEntity
 {
     public Guid Id { get; }
-    public Patron Patron;
-    public double Amount;
-    public DateTime DueDate;
-    public DateTime? PaidDate;
-    public bool Paid;
+
+    private Patron _patron;
+    private double _amount;
+    private DateTime _dueDate;
+    private DateTime? _paidDate;
+    private bool _paid;
 
     public Fine(Patron patron, double amount, DateTime dueDate)
     {
         Id = Guid.NewGuid();
-        Patron = patron;
-        Amount = amount;
-        DueDate = dueDate;
+        _patron = patron;
+        _amount = amount;
+        _dueDate = dueDate;
+        _paid = false;
+    }
+
+    public double Amount
+    {
+        get => _amount;
+        set => _amount = value;
+    }
+
+    public DateTime DueDate
+    {
+        get => _dueDate;
+        set => _dueDate = value;
+    }
+
+    public DateTime? PaidDate
+    {
+        get => _paidDate;
+        set => _paidDate = value;
+    }
+
+    public bool Paid
+    {
+        get => _paid;
+        set => _paid = value;
     }
 
     public void PayFine()
     {
-        Paid = true;
-        PaidDate = DateTime.Now;
+        _paid = true;
+        _paidDate = DateTime.Now;
     }
 
     public override string ToString()
     {
         return $"Fine:\n"
-            + $"Patron: \t {Patron}\n===================\n"
+            + $"Patron: \t {_patron.Name}\n===================\n"
             + $"Amount: {Amount}\n"
             + $"DueDate: {DueDate:yyyy-MM-dd}\n"
-            + $"PaidDate: {PaidDate:yyyy-MM-dd}\n"
+            + $"PaidDate: {(PaidDate.HasValue ? PaidDate.Value.ToString("yyyy-MM-dd") : "Not Paid")}\n"
             + $"Paid: {Paid}\n";
     }
 }
