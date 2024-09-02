02f9d9c65ef78bca9efd71a584c5abc200af030d
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2024-09-02 04:01:30 -0400

    refactor: patron class to improve encapsulation

--- Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/Patron.cs
+++ Opcion1LosChambos/Opcion1LosChambosApp/Entities/Concretes/Patron.cs
@@ -3,23 +3,44 @@ namespace LosChambos.Entities.Concretes;
 public class Patron : IEntity
 {
     public Guid Id { get; }
-    public string Name { get; set;}
-    public int MembershipNumber { get; set;}
-    public string ContactDetails { get; set;}
+
+    private string _name;
+    private int _membershipNumber;
+    private string _contactDetails;
 
     public Patron(string name, int membershipNumber, string contactDetails)
     {
         Id = Guid.NewGuid();
-        Name = name;
-        MembershipNumber = membershipNumber;
-        ContactDetails = contactDetails;
+        _name = name;
+        _membershipNumber = membershipNumber;
+        _contactDetails = contactDetails;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int MembershipNumber
+    {
+        get => _membershipNumber;
+        set => _membershipNumber = value;
+    }
+
+    public string ContactDetails
+    {
+        get => _contactDetails;
+        set => _contactDetails = value;
     }
 
     public void BorrowBook(Book book)
-    {}
+    {
+    }
 
     public void ReturnBook(Book book)
-    {}
+    {
+    }
 
     public override string ToString()
     {
