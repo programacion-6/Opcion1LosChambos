31053e4bb83f93b76b827d1abd5307516c3c7c7f
SebastianBarra <barra20sebastian02p19@gmail.com>
2024-09-02 08:41:42 -0400

    hotfix: fix book listing

--- Opcion1LosChambos/Opcion1LosChambosApp/Validators/Concretes/BookValidator.cs
+++ Opcion1LosChambos/Opcion1LosChambosApp/Validators/Concretes/BookValidator.cs
@@ -8,9 +8,9 @@ public class BookValidator : Validator<Book>
     protected override void InitializeValidations()
     {
         Validations.Add("Title is required.", book => !string.IsNullOrEmpty(book.Title));
-        Validations.Add("Author is required.", book => !string.IsNullOrEmpty(book.Author) && book.Author.All(char.IsLetter));
+        Validations.Add("Author is required.", book => !string.IsNullOrEmpty(book.Author) && book.Author.All(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
         Validations.Add("ISBN is required.", book => !string.IsNullOrEmpty(book.ISBN));
-        Validations.Add("Genre is required.", book => !string.IsNullOrEmpty(book.Genre) && book.Genre.All(char.IsLetter));
+        Validations.Add("Genre is required.", book => !string.IsNullOrEmpty(book.Genre) && book.Genre.All(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
         Validations.Add("Publication year must be positive.", book => book.PublicationYear > 0);
     }
 
